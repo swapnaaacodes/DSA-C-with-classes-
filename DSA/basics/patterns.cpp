66b7413cef6d9475bf916f26400ec9fd51ac1655
swapnaaacodes <126751627+swapnaaacodes@users.noreply.github.com>
2026-06-25 20:16:09 +0530

    pattern pt.2

--- DSA/basics/patterns.cpp
+++ DSA/basics/patterns.cpp
@@ -1,6 +1,54 @@
 #include<iostream>
 using namespace std;
 
+//pattern 9 
+void print8(int n){
+    for(int i=1; i<=2*n-1;i++){
+        int stars = i;
+        if(i>n) stars = 2*n-i;
+        for(int j=1; j<=stars;j++){
+            cout <<"*";
+        }
+        cout<<endl;
+    }
+}
+
+//pattern 8
+void print7(int n){
+    for(int i=0; i<n; i++){
+        //space
+        for(int j=0; j<i; j++){
+            cout<<" ";
+        }
+        //star
+        for(int k=0; k<2*n-(2*i+1); k++){
+            cout<<"*";
+        }
+        //space
+        for(int l=0; l<i; l++){
+            cout<<" ";
+        }
+        cout << endl;
+    }
+}
+//pattern 7
+void print6(int n){
+    for(int i=0; i<n; i++){
+        //space
+        for(int j=0; j<n-i-1; j++){
+            cout<<" ";
+        }
+        //star
+        for(int k=0; k<2*i+1; k++){
+            cout<<"*";
+        }
+        //space
+        for(int l=0; l<n-i-1; l++){
+            cout<<" ";
+        }
+        cout << endl;
+    }
+}
 //pattern 6
 void print5(int n){
     for(int i =1; i<=n; i++){
@@ -37,7 +85,6 @@ void print2(int n){
         cout << endl;
     }
 }
-
 //pattern-1
 void print1(int n){
     for(int i=0; i<n; i++){
@@ -50,11 +97,14 @@ void print1(int n){
 
 
 int main(){
-    int t = 2;
-    for(int i =0; i<t; i++){
-        int n;
-    }
-    print5(3);
-    print5(5);
+
+    int n = 5;
+    // int t = 1;
+    // for(int i =0; i<t; i++){
+    //     int n;
+    //     print6(5); 
+    //     print7(5);
+    // }
+    print8(5);
     return 0;
 }
